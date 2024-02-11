87ad9406e8d5d3e243113575954b90714740025d
EvilNotAl0ne <sacha1234sacha@mail.ru>
2024-02-11 18:37:08 +0300

    o_O

--- autopark/forms.py
+++ autopark/forms.py
@@ -1,14 +1,37 @@
 import datetime
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 
 
 from .models import Driver, Car, CarBrand, Client
 from AutoparkProject.settings import DATE_INPUT_FORMATS
 
-class DriverModelForm(forms.ModelForm):
+class RegistrationFrom(UserCreationForm):
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторить пароль', widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+        
+        labels = {
+        'username': 'Логин',
+        'email': 'Эл. почта',
+        'password1': 'Пароль',
+        'password2': 'Повторить пароль',
+        }
+        help_text = {
+        'username': '',
+        'password1': '',
+        }
+
+class DriverForm(forms.ModelForm):
+    birthday = forms.DateField(label='Дата рождения', widget=forms.DateInput(format='%d.%m.%Y'), input_formats=['%d.%m.%Y'])
     class Meta:
         model = Driver
-        exclude = ["is_available"]
-        
+        exclude = ["user", "is_available"]
+
+
+
 
